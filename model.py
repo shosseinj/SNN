54bea6405e5ca658d5eeb0b88c9d6b353da51c37
Hossein Jafari <hossein97jafari@gmail.com>
2025-09-04 14:53:41 +0330

    code worked

--- model.py
+++ model.py
@@ -18,8 +18,9 @@ from keras import optimizers
 import numpy as np
 from keras import backend as K
 from keras import regularizers
-
-
+from utils import *
+tf.keras.backend.set_floatx('float64')
+from tensorflow.keras.layers import Lambda
 
 
 from tensorflow.keras.models import Sequential
@@ -147,19 +148,6 @@ def VGG16(input_shape=(32, 32, 3), classes=10, weights_path='weights/cifar10vgg.
 
 
 
-
-
-
-###############################
-
-import logging
-import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, Input, Dense, MaxPool2D, Flatten, Dropout, BatchNormalization
-from tensorflow.keras.models import Model
-from utils import *
-tf.keras.backend.set_floatx('float64')
-
-
 class SpikingDense(tf.keras.layers.Layer):
     def __init__(self, units, name, X_n=1, outputLayer=False, robustness_params={}, input_dim=None,
                  kernel_regularizer=None, kernel_initializer=None):
@@ -178,7 +166,9 @@ class SpikingDense(tf.keras.layers.Layer):
         # In case this is the first dense layer after Flatten layer.
         if input_dim[-1] is None: input_dim=(None, self.input_dim)
         self.kernel = self.add_weight(shape=(input_dim[-1], self.units), name='kernel', regularizer=self.regularizer, initializer=self.initializer)
-        self.D_i = self.add_weight(shape=(self.units), initializer=tf.constant_initializer(0), name='D_i')
+        # self.D_i = self.add_weight(shape=(self.units),  initializer=tf.constant_initializer(0), name='D_i')
+        self.D_i = self.add_weight(shape=(self.units,), initializer=tf.constant_initializer(0), name='D_i')
+
         self.built = True
     
     def set_params(self, t_min_prev, t_min):
@@ -206,7 +196,7 @@ class SpikingDense(tf.keras.layers.Layer):
     
 class SpikingConv2D(tf.keras.layers.Layer):
     def __init__(self, filters, name, X_n=1, padding='same', kernel_size=(3,3), robustness_params={},
-                 kernel_regularizer=None, kernel_initializer=None,  noise=0):
+                 kernel_regularizer=None, kernel_initializer=None):
         self.filters=filters
         self.kernel_size=kernel_size
         self.padding=padding
@@ -215,8 +205,10 @@ class SpikingConv2D(tf.keras.layers.Layer):
         self.B_n = (1 + 0.5) * X_n
         self.t_min_prev, self.t_min, self.t_max=0, 0, 1
         self.robustness_params=robustness_params
-        self.noise = noise
         self.alpha = tf.cast(tf.fill((filters, ), 1), dtype=tf.float64)
+
+
+        self.noise=0 # by hossein
         super(SpikingConv2D, self).__init__(name=name)
     
     def build(self, input_dim):
@@ -228,6 +220,9 @@ class SpikingConv2D(tf.keras.layers.Layer):
         # When fusing a batch normalization layer with the next convolutional layer where padding=='same', some of the biases in scaled ReLU network are changed, leading to 9 different values.
         self.D_i = self.add_weight(shape=(9, self.filters), initializer=tf.constant_initializer(0), name='D_i')
         self.built = True
+    
+
+
         self.BN = 0 #by hoss
         self.BN_before_ReLU = 0
     def set_params(self, t_min_prev, t_min):
@@ -239,53 +234,6 @@ class SpikingConv2D(tf.keras.layers.Layer):
         self.t_max=tf.Variable(tf.constant(t_min+self.B_n, dtype=tf.float64), trainable=False, name='t_max')
         return t_min, t_min+self.B_n
 
-    # def call(self, tj):
-    #     """
-    #     Input spiking times tj, output spiking times ti. 
-    #     """
-    #     # Image size in case of padding='same' or padding='valid'.
-    #     padding_size, image_same_size = int(self.padding=='same')*(self.kernel_size[0]//2), tf.shape(tj)[1] 
-    #     image_valid_size = image_same_size - self.kernel_size[0]+1
-    #     # Pad input with t_min value, which is equivalent with 0 in ReLU network.
-    #     tj=tf.pad(tj, tf.constant([[0, 0], [padding_size, padding_size,], [padding_size, padding_size], [0, 0]]), constant_values=self.t_min)
-    #     # Extract image patches of size (kernel_size, kernel_size). call_spiking function will be called for different patches in parallel.  
-    #     tj = tf.image.extract_patches(tj, sizes=[1, self.kernel_size[0], self.kernel_size[1], 1], strides=[1, 1, 1, 1], rates=[1, 1, 1, 1], padding='VALID')
-    #     # We reshape input and weights in order to utilize the same function as for the fully-connected layer.
-    #     W = tf.reshape(self.kernel, (-1, self.filters))
-    #     if self.padding=='valid' or self.BN!=1 or self.BN_before_ReLU==1: 
-    #         # In this case the threshold is the same for whole input image.
-    #         tj = tf.reshape(tj, (-1, tf.shape(W)[0]))
-    #         ti = call_spiking(tj, W, self.D_i[0], self.t_min_prev, self.t_min, self.t_max)
-    #         # Layer output is reshaped back.
-    #         if self.padding=='valid':
-    #             ti = tf.reshape(ti, (-1, image_valid_size, image_valid_size, self.filters))
-    #         else:
-    #             ti = tf.reshape(ti, (-1, image_same_size, image_same_size, self.filters))
-    #     else:
-    #         # In this case there are 9 different thresholds for 9 different image partitions.
-    #         tj_partitioned = [tj[:, 1:-1, 1:-1, :], tj[:, :1, :1, :], tj[:, :1, 1:-1, :], tj[:, :1, -1:, :], tj[:, 1:-1, -1:, :], tj[:, -1:, -1:, :] , tj[:, -1:, 1:-1, :], tj[:, -1:, :1, :], tj[:, 1:-1, :1, :]]
-    #         ti_partitioned=[]
-    #         for i, tj_part in enumerate(tj_partitioned):
-    #             # Iterate over 9 different partitions and call call_spiking with different threshold value.
-    #             tj_part = tf.reshape(tj_part, (-1, tf.shape(W)[0]))
-    #             ti_part = call_spiking(tj_part, W, self.D_i[i], self.t_min_prev, self.t_min, self.t_max, noise=self.noise)
-    #             # Partitions are reshaped back.
-    #             if i==0: ti_part=tf.reshape(ti_part, (-1, image_valid_size, image_valid_size, self.filters))
-    #             if i in [1, 3, 5, 7]: ti_part=tf.reshape(ti_part, (-1, 1, 1, self.filters))
-    #             if i in [2, 6]: ti_part=tf.reshape(ti_part, (-1, 1, image_valid_size, self.filters))
-    #             if i in [4, 8]: ti_part=tf.reshape(ti_part, (-1, image_valid_size, 1, self.filters))
-    #             ti_partitioned.append(ti_part) 
-    #         # Partitions are concatenated to create a complete output.
-    #         if image_valid_size!=0:
-    #             ti_top_row = tf.concat([ti_partitioned[1], ti_partitioned[2], ti_partitioned[3]], axis=2)
-    #             ti_middle = tf.concat([ti_partitioned[8], ti_partitioned[0], ti_partitioned[4]], axis=2)
-    #             ti_bottom_row = tf.concat([ti_partitioned[7], ti_partitioned[6], ti_partitioned[5]], axis=2)
-    #             ti = tf.concat([ti_top_row, ti_middle, ti_bottom_row], axis=1)         
-    #         else:
-    #             ti_top_row = tf.concat([ti_partitioned[1], ti_partitioned[3]], axis=2)
-    #             ti_bottom_row = tf.concat([ti_partitioned[7], ti_partitioned[5]], axis=2)
-    #             ti = tf.concat([ti_top_row, ti_bottom_row], axis=1)   
-    #     return ti
     def call(self, tj):
         """
         Input spiking times tj, output spiking times ti. 
@@ -302,7 +250,6 @@ class SpikingConv2D(tf.keras.layers.Layer):
         if self.padding=='valid' or self.BN!=1 or self.BN_before_ReLU==1: 
             # In this case the threshold is the same for whole input image.
             tj = tf.reshape(tj, (-1, tf.shape(W)[0]))
-            # FIX: Add robustness_params parameter
             ti = call_spiking(tj, W, self.D_i[0], self.t_min_prev, self.t_min, self.t_max, self.robustness_params)
             # Layer output is reshaped back.
             if self.padding=='valid':
@@ -316,8 +263,7 @@ class SpikingConv2D(tf.keras.layers.Layer):
             for i, tj_part in enumerate(tj_partitioned):
                 # Iterate over 9 different partitions and call call_spiking with different threshold value.
                 tj_part = tf.reshape(tj_part, (-1, tf.shape(W)[0]))
-                # FIX: Add robustness_params parameter
-                ti_part = call_spiking(tj_part, W, self.D_i[i], self.t_min_prev, self.t_min, self.t_max, self.robustness_params, noise=self.noise)
+                ti_part = call_spiking(tj_part, W, self.D_i[i], self.t_min_prev, self.t_min, self.t_max, self.robustness_params)
                 # Partitions are reshaped back.
                 if i==0: ti_part=tf.reshape(ti_part, (-1, image_valid_size, image_valid_size, self.filters))
                 if i in [1, 3, 5, 7]: ti_part=tf.reshape(ti_part, (-1, 1, 1, self.filters))
@@ -335,6 +281,7 @@ class SpikingConv2D(tf.keras.layers.Layer):
                 ti_bottom_row = tf.concat([ti_partitioned[7], ti_partitioned[5]], axis=2)
                 ti = tf.concat([ti_top_row, ti_bottom_row], axis=1)   
         return ti
+
 
 class ModelTmax(tf.keras.Model):
     def __init__(self, **kwargs):
@@ -372,55 +319,7 @@ class ModelTmax(tf.keras.Model):
         return {m.name: m.result() for m in self.metrics}
 
 
-def create_vgg_model_ReLU(layers2D, kernel_size, layers1D, data, BN, dropout=0, optimizer='adam',
-                          kernel_regularizer=None, kernel_initializer='glorot_uniform'):
-    """
-    Create a VGG-like ReLU network for various dataset.
-    """
-    inputs = Input(shape=data.input_shape)
-    i_conv = 0
-    i_bn = 0
-    i_dense = 0
-    for k, f in enumerate(layers2D):
-        if f!='pool':
-            i_conv += 1
-            if k==0:
-                x=Conv2D(f,  kernel_size, padding='same', activation=None,
-                         kernel_regularizer=kernel_regularizer,
-                         kernel_initializer=kernel_initializer,
-                         name='conv2d_'+str(i_conv))(inputs)
-            else:
-                x=Conv2D(f,  kernel_size, padding='same', activation=None,
-                         kernel_regularizer=kernel_regularizer,
-                         kernel_initializer=kernel_initializer,
-                         name='conv2d_'+str(i_conv))(x)
-            x = tf.keras.layers.Activation('relu')(x)
-            if BN:
-                i_bn += 1
-                x=BatchNormalization(name='batch_normalization_'+str(i_bn))(x)
-            x = Dropout(dropout)(x)
-        else:
-            x=MaxPool2D()(x)
-    x=Flatten()(x)
-    for j, d in enumerate(layers1D):
-        i_dense +=1
-        x=Dense(d, activation=None,
-                kernel_regularizer=kernel_regularizer,
-                kernel_initializer=kernel_initializer,
-                name='dense_'+str(i_dense))(x)
-        x = tf.keras.layers.Activation('relu')(x)
-        if BN:
-            i_bn += 1
-            x=BatchNormalization(name='batch_normalization_'+str(i_bn))(x)
-        x = Dropout(dropout)(x)
-    i_dense +=1
-    outputs=Dense(data.num_of_classes, activation=None,
-                  kernel_regularizer=kernel_regularizer,
-                  #kernel_initializer=kernel_initializer, #logits - should be standard glorot
-                  name='dense_' +str(i_dense))(x)
-    model = Model (inputs=inputs, outputs=outputs)
-    model.compile(metrics=['accuracy'], loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True), optimizer=optimizer)
-    return model
+
 
 
 def create_vgg_model_SNN(layers2D, kernel_size, layers1D, data, optimizer, X_n=1000,
@@ -434,14 +333,18 @@ def create_vgg_model_SNN(layers2D, kernel_size, layers1D, data, optimizer, X_n=1
                        kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer,
                        padding='same', kernel_size=kernel_size,
                        robustness_params=robustness_params)(tj)
-    min_ti.append(tf.reduce_min(ti))
+    # min_ti.append(tf.reduce_min(ti))
+    min_ti_layer = Lambda(lambda x: tf.reduce_min(x))
+    min_ti.append(min_ti_layer(ti))
     j, image_size =1, data.input_shape[0]
     for f in layers2D[1:]:
         if f!='pool':
             ti = SpikingConv2D(f, 'conv2d_' +str(1+j), (X_n[j] if type(X_n)==list else X_n),
                                kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer,
                                padding='same', kernel_size=kernel_size, robustness_params=robustness_params)(ti)
-            min_ti.append(tf.reduce_min(ti))
+            # min_ti.append(tf.reduce_min(ti))
+            min_ti_layer = Lambda(lambda x: tf.reduce_min(x))
+            min_ti.append(min_ti_layer(ti))
             j=j+1
         else:
             ti, image_size=-MaxMinPool2D()(-ti), image_size//2
@@ -450,14 +353,18 @@ def create_vgg_model_SNN(layers2D, kernel_size, layers1D, data, optimizer, X_n=1
     ti =SpikingDense(layers1D[0], 'dense_'+str(i_dense), (X_n[j] if type(X_n)==list else X_n),
                      kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer,
                      robustness_params=robustness_params, input_dim=(image_size**2)*layers2D[-2])(ti)
-    min_ti.append(tf.reduce_min(ti))
+    # min_ti.append(tf.reduce_min(ti))
+    min_ti_layer = Lambda(lambda x: tf.reduce_min(x))
+    min_ti.append(min_ti_layer(ti))
     j, k=j+1, 0
     for k, d in enumerate(layers1D[1:]):
         i_dense +=1
         ti =SpikingDense(d, 'dense_'+str(i_dense), (X_n[j] if type(X_n)==list else X_n),
                          kernel_regularizer=kernel_regularizer, kernel_initializer=kernel_initializer,
                          robustness_params=robustness_params)(ti)
-        min_ti.append(tf.reduce_min(ti))
+        # min_ti.append(tf.reduce_min(ti))
+        min_ti_layer = Lambda(lambda x: tf.reduce_min(x))
+        min_ti.append(min_ti_layer(ti))
         j+=1
     i_dense +=1
     outputs =SpikingDense(data.num_of_classes, 'dense_'+str(i_dense), outputLayer=True,
@@ -468,39 +375,9 @@ def create_vgg_model_SNN(layers2D, kernel_size, layers1D, data, optimizer, X_n=1
     return model
 
 
-def create_fc_model_ReLU(layers = 2, optimizer='adam', N_hid=340, N_in=784, N_out=10):
-    """
-    Create a 2-layer fully-connected ReLU network to for MNIST dataset.
-    """
-    N = lambda l: (N_hid[l-1] if type(N_hid)==list else N_hid)
-    inputs = Input(shape=(N_in))
-    x = Dense(N(1), activation=None, name='dense_1')(inputs)
-    x = tf.keras.layers.Activation('relu')(x)
-    for i in range(layers-2):
-        x = Dense(N(i+2), activation=None, name='dense_'+str(i+2))(x)
-        x = tf.keras.layers.Activation('relu')(x)
-    outputs = Dense(N_out, activation=None, name='dense_output')(x)
-    model = Model (inputs=inputs, outputs=outputs)
-    model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True), optimizer=optimizer, metrics=["categorical_accuracy"])
-    return model
 
 
-def create_fc_model_SNN(layers, optimizer, X_n=1000, robustness_params={}, N_hid=340, N_in=784, N_out=10):
-    """
-    Create 2-layer fully connected network. Tested on MNIST dataset.
-    """
-    N = lambda l: (N_hid[l-1] if type(N_hid)==list else N_hid)
-    min_ti=[]
-    tj = Input(shape=N_in)
-    ti = SpikingDense(N(1), 'dense_1', (X_n[0] if type(X_n)==list else X_n), robustness_params=robustness_params)(tj)
-    min_ti.append(tf.reduce_min(ti))
-    for i in range(layers-2):
-        ti = SpikingDense(N(i+2), 'dense_' + str(i+2), (X_n[1+i] if type(X_n)==list else X_n), robustness_params=robustness_params)(ti)
-        min_ti.append(tf.reduce_min(ti))
-    outputs = SpikingDense(N_out, 'dense_output', outputLayer=True, robustness_params=robustness_params)(ti)
-    model = ModelTmax (inputs=tj, outputs=[outputs, min_ti])
-    model.compile(metrics=['accuracy'], loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True), optimizer=optimizer)
-    return model
+
 
 
 def call_spiking(tj, W, D_i, t_min_prev, t_min, t_max, robustness_params):
